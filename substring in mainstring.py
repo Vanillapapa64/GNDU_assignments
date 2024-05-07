cf0fd13e9ce1895a117598fe4bc7a0e60b12d0ec
Navkirat singh <132665095+Vanillapapa64@users.noreply.github.com>
2024-05-07 20:33:56 +0530

    Create substring in mainstring.py

---
+++ substring in mainstring.py
@@ -0,0 +1,6 @@
+mainstring=input('enter the main string:')
+substring=input("enter the substring: ")
+if substring in mainstring:
+    print("substring found in mainstring")
+else:
+    print("substring not found in mainstring")
